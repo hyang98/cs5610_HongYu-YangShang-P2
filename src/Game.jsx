ec43b21a4abd9509e02bb9b47741e584931da900
Hong Yang <hongyue98@hotmail.com>
2023-11-10 15:14:59 -0800

    fixed routing issues and added word list

---
+++ src/Game.jsx
@@ -0,0 +1,11 @@
+function Game() 
+{
+    return (
+      <div>
+        <h1>Welcome to Wordle Game</h1>
+        <p>Select a difficulty level:</p>
+      </div>
+    );
+}
+
+export default Game
---
+++ src/wordStorage.js
@@ -0,0 +1,7 @@
+const words = 
+{
+    easy: [About, Apple, Bread, Cable, Lunch, Money, Queen, Roman, Share, Sharp, Under],
+    hard: [Azure, Craic, Ducat, Enoki, Loath, Rural, Slake, Spate, Tares, Quash]
+}
+
+export default words
