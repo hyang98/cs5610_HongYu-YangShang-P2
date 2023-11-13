80bc71da01b89199dc93a96a47982898448ca97f
Hong Yang <hongyue98@hotmail.com>
2023-11-13 15:28:04 -0800

    added checks for attempts guessed

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,11 +2,7 @@ import { useContext, useState } from "react";
 import './Game.css';
 import InputGuess from "./InputGuess";
 
-function Game() 
-{
-  // const { difficulty } = location.difficulty;
-  // console.log(difficulty);
-  // setAttempts(difficulty === 'normal' ? 6:3);
+function Game() {
   const [attempts, setAttempts] = useState(6);
   const [input, setInput] = useState('');
   const [gameDone, setGameDone] = useState(false);
@@ -17,48 +13,45 @@ function Game()
   }
 
   function submitInput() {
-    const newInputHistory = [{input: input}];
-    for(let i = 0; i < inputHistory.length; i++) {
-      newInputHistory.push(inputHistory[i]);
+    if (input.trim() !== '' && attempts > 0) {
+      const newInputHistory = [{ input: input }];
+      for (let i = 0; i < inputHistory.length; i++) {
+        newInputHistory.push(inputHistory[i]);
+      }
+      setInputHistory(newInputHistory);
+      setInput('');
+      setAttempts((prevAttempts) => prevAttempts - 1);
+  
+      if (attempts - 1 === 0) {
+        setGameDone(true);
+      }
     }
-    setInputHistory(newInputHistory);
-    setInput('');
-    console.log(inputHistory);
   }
 
-  const inputHistoryComponents = [];
-  for(let i = 0; i < inputHistory.length; i++) {
-    const inputItem = inputHistory[i];
-    const inputComponent = (<InputGuess input={inputItem.input}/>)
-    inputHistoryComponents.push(inputComponent);
-  }
+  const inputHistoryComponents = inputHistory.map((inputItem, index) => (
+    <InputGuess key={index} input={inputItem.input} />
+  ));
 
-
-
-
-
-
-
-    return (
-      <div>
-        <div className = "container">
-          <div className="homeContainer">
-            <div className = "container"><h1>Wordle</h1></div>
-            <h2>You have {attempts} attempts remaining !</h2>
-            <div className = "container">
-              <p>Please make the Guess:</p>
-            </div>
-            <div className = "container">
-              <input onInput={(event) => inputGuess(event)} value = {input}></input>
-              <div className="buttonLink">
-                <button onClick={() => submitInput()}>Submit</button>
-              </div>
+  return (
+    <div>
+      <div className = "container">
+        <div className="homeContainer">
+          <div className = "container"><h1>Wordle</h1></div>
+          <h2>You have {attempts} attempts remaining !</h2>
+          <div className = "container">
+            <p>Please make the Guess:</p>
+          </div>
+          <div className = "container">
+            <input onInput={(event) => inputGuess(event)} value = {input}></input>
+            <div className="buttonLink">
+              <button onClick={() => submitInput()}>Submit</button>
             </div>
           </div>
         </div>
-        <div className="previousResult">{inputHistoryComponents}</div>;
       </div>
-    );
+      <div className="previousResult">{inputHistoryComponents}</div>;
+    </div>
+  );
 }
 
 export default Game
